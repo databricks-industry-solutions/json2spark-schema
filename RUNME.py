cdadc8f648be1f24281dbc05c6832e55fe3f7d97
zavoraad <zavoraad>
2023-05-02 13:20:15 +0000

    Job cluster info

--- RUNME.py
+++ RUNME.py
@@ -1,24 +1,24 @@
 # Databricks notebook source
 # MAGIC %md This notebook sets up the companion cluster(s) to run the solution accelerator on a Databricks Workspace. It also creates the Workflow to illustrate the order of execution. Happy exploring! 
 # MAGIC 🎉
-# MAGIC 
+# MAGIC
 # MAGIC **Steps**
 # MAGIC 1. Simply attach this notebook to a cluster and hit Run-All for this notebook. A multi-step job and the clusters used in the job will be created for you and hyperlinks are printed on the last block of the notebook. 
-# MAGIC 
+# MAGIC
 # MAGIC 2. Run the accelerator notebooks: Feel free to explore the multi-step job page and **run the Workflow**, or **run the notebooks interactively** with the cluster to see how this solution accelerator executes. 
-# MAGIC 
+# MAGIC
 # MAGIC     2a. **Run the Workflow**: Navigate to the Workflow link and hit the `Run Now` 💥. 
 # MAGIC   
 # MAGIC     2b. **Run the notebooks interactively**: Attach the notebook with the cluster(s) created and execute as described in the `job_json['tasks']` below.
-# MAGIC 
+# MAGIC
 # MAGIC **Prerequisites** 
 # MAGIC 1. You need to have cluster creation permissions in this workspace.
-# MAGIC 
+# MAGIC
 # MAGIC 2. In case the environment has cluster-policies that interfere with automated deployment, you may need to manually create the cluster in accordance with the workspace cluster policy. The `job_json` definition below still provides valuable information about the configuration these series of notebooks should run with. 
-# MAGIC 
+# MAGIC
 # MAGIC **Notes**
 # MAGIC 1. The pipelines, workflows and clusters created in this script are not user-specific. Keep in mind that rerunning this script again after modification resets them for other users too.
-# MAGIC 
+# MAGIC
 # MAGIC 2. If the job execution fails, please confirm that you have set up other environment dependencies as specified in the accelerator notebooks. Accelerators may require the user to set up additional cloud infra or secrets to manage credentials. 
 
 # COMMAND ----------
@@ -30,8 +30,8 @@
 
 # MAGIC %sh -e
 # MAGIC #Download to DBFS storage
-# MAGIC mkdir -p /dbfs/databricks-industry-solutions/hls-payer-mrf-sparkstreaming/
-# MAGIC wget https://github.com/databricks-industry-solutions/hls-payer-mrf-sparkstreaming/releases/download/0.3.5v/payer-mrf-streamsource-0.3.5.jar -O /dbfs/databricks-industry-solutions/hls-payer-mrf-sparkstreaming/payer-mrf-streamsource-0.3.5.jar
+# MAGIC mkdir -p /dbfs/databricks-industry-solutions/json2spark-schema/
+# MAGIC wget https://github.com/databricks-industry-solutions/json2spark-schema/releases/download/v0.0.1/json2spark-schema-0.0.1.jar -O /dbfs/databricks-industry-solutions/json2spark-schema/json2spark-schema-0.0.1.jar
 
 # COMMAND ----------
 
@@ -48,45 +48,43 @@ job_json = {
         },
         "tasks": [
             {
-                "job_cluster_key": "payer_mrf_cluster",
+                "job_cluster_key": "json2spark_cluster",
                 "libraries": [],
                 "notebook_task": {
                     "notebook_path": f"00_README"
                 },
-                "task_key": "payer_mrf_00",
+                "task_key": "json2spark_00",
                 "description": ""
             },
             {
-                "job_cluster_key": "payer_mrf_cluster",
+                "job_cluster_key": "json2spark_cluster",
                 "notebook_task": {
-                    "notebook_path": f"01_payer_mrf_demo"
+                    "notebook_path": f"json2spark_demo"
                 },
                 "libraries": [
                     {
-                        "jar": "dbfs:/databricks-industry-solutions/hls-payer-mrf-sparkstreaming/payer-mrf-streamsource-0.3.5.jar"
+                        "jar": "dbfs:/databricks-industry-solutions/json2spark-schema/json2spark-schema-0.0.1.jar"
                     }
                 ],
                 "depends_on": [
                     {
-                        "task_key": "payer_mrf_00"
+                        "task_key": "json2spark_00"
                     }
                 ],
-                "task_key": "payer_mrf_01",
+                "task_key": "json2spark_01",
                 "description": ""
             }
         ],
         "job_clusters": [
             {
-                "job_cluster_key": "payer_mrf_cluster",
+                "job_cluster_key": "json2spark_cluster",
                 "new_cluster": {
-                    "spark_version": "10.4.x-scala2.12",
+                    "spark_version": "11.3.x-scala2.12",
                 "spark_conf": {
-                    "spark.rpc.message.maxSize": "1024",
-                    "spark.driver.cores": "3", # 1 reader, 1 offset writer, 1 for spark tasks
                     "spark.executor.cores": "2",
-                    "spark.executor.instances": "4"
+                    "spark.executor.instances": "2"
                     },
-                    "num_workers": 4,
+                    "num_workers": 2,
                     "node_type_id": {"AWS": "i3.xlarge", "MSA": "Standard_L4s", "GCP": "n1-highmem-4"},
                     "custom_tags": {
                         "usage": "solacc_testing"
